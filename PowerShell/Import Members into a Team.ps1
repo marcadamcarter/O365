30d59e0dc16399266588a634f71bf8a9143d4bf4
marc carter <marc.a.carter@gmail.com>
2021-06-09 11:35:49 -0500

    Create Import Members into a Team.ps1

---
+++ PowerShell/Import Members into a Team.ps1
@@ -0,0 +1,15 @@
+# Team must be manually created in order to import users
+# Query Microsoft Teams
+Import-Module AzureAD -Scope CurrentUser # Import necessary PowerShell Module
+Connect-AzureAD 
+
+# Display a list of Teams
+Get-AzureADGroup -Filter "startswith(DisplayName,'Niffty Network')"
+
+# Grab new Team ObjectId
+$objID = Get-AzureADGroup -ObjectId <GUID> | Select-Object ObjectId
+
+# Grab members of an old Team and import into new Team
+# Refer to CSV file in https://github.com/marcadamcarter/O365/blob/794444e9d3d74d115b95517f39b601bb247737ef/PowerShell/Get%20Microsoft%20Teams%20Memberships.ps1#L12
+$members = Import-Csv -Path 'C:\Users\joe.user\Downloads\niffty network team bravo.csv'
+foreach($mbr in $members){ Add-AzureADGroupMember -ObjectID ($objID).ObjectId -RefObjectId (Get-AzureADUser -ObjectID $mbr.MailNickName).ObjectID }
